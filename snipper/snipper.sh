b96444156f907be900c7ade75c6f554940d6e657
Mesut Tasci <slackermesut@gmail.com>
2016-09-22 19:32:45 +0300

    Initial commit

---
+++ snipper/snipper.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+USERNAME=$BITBUCKET_USER
+PASSWORD=$BITBUCKET_PASSWORD
+
+SNIPPET_DIR="$HOME/.snippets"
+
+BASE_API_URI='https://api.bitbucket.org/2.0'
+
+# Set 0 for https
+CLONE_WITH_SSH=1
+
+
+function help {
+cat << EOF
+Usage of $0:
+    ls
+        List all snippets
+    find
+        Search text in local snippets
+    sync
+        Sync local snippet repos from bitbucket.org
+EOF
+}
+
+function createTempFile {
+    echo $(mktemp)
+}
+
+function slugify {
+    echo "$@" | iconv -t ascii//TRANSLIT | sed -r 's/[^a-zA-Z0-9]+/-/g' | sed -r 's/^-+\|-+$//g' | tr A-Z a-z
+}
+
+function downloadResource {
+    tmpFile=$(createTempFile)
+    curl --silent -o $tmpFile -u $USERNAME:$PASSWORD $1
+    echo $tmpFile
+}
+
+function cloneGitSnippet {
+    git clone $1 $2
+}
+
+function updateGitSnippets {
+    #echo $cloneDir
+    for dir in $SNIPPET_DIR/*/*/ ; do
+        echo "Updating: $dir"
+        git --git-dir=$dir/.git pull
+    done
+}
+
+function syncSnippets {
+    # Sync remote with local
+    # clone new snippets and update existing snippets
+
+    tmpFile=$(downloadResource "$BASE_API_URI/snippets/$USERNAME")
+    fileContent=$(cat $tmpFile)
+
+    i=0
+    echo $fileContent | jq -r ".values[].links.clone[$CLONE_WITH_SSH].href" | \
+    while read cloneUri
+     do
+        snippetId=$(echo $fileContent | jq -r --arg i "$i" ".values[$i].id")
+        snippetOwner=$(echo $fileContent | jq -r --arg i "$i" ".values[$i].owner.username")
+
+        # Dont use title because title changable from web
+        #title=$(echo $fileContent | jq -r --arg i "$i" ".values[$i].title")
+        #title=$(slugify $title)
+        cloneTo="$SNIPPET_DIR/$snippetOwner/$snippetId"
+
+        if [ ! -d $cloneTo ]; then
+            echo "Downloading new snippet from bitbucket: $title"
+            cloneGitSnippet $cloneUri $cloneTo
+        else
+            updateGitSnippets
+        fi
+
+        i=${i+1}
+    done
+}
+
+function listRemoteSnippets {
+    tmpFile=$(downloadResource "$BASE_API_URI/snippets/$USERNAME")
+    fileContent=$(cat $tmpFile)
+    i=0
+    echo $fileContent | jq -r ".values[].title" | \
+    while read title
+    do
+        snippetId=$(echo $fileContent | jq -r --arg i "$i" ".values[$i].id")
+        echo "[$snippetId] $title"
+        i=${i+1}
+    done
+}
+
+
+while [[ $# -gt 0 ]]; do
+
+case $1 in
+    help|--help|-h)
+    help
+    shift # past argument
+    ;;
+    ls)
+    ACTION='list'
+    shift # past argument
+    ;;
+    sync)
+    ACTION='sync'
+    shift # past argument
+    ;;
+    *)
+    # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+echo $ACTION
+
+case $ACTION in
+    'list')
+        listRemoteSnippets
+        ;;
+    'sync')
+        syncSnippets
+        ;;
+    'find')
+        echo 'FIND FILES'
+        ;;
+esac
