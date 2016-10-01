8c872a80b5a06f20b56613c816039d40b5e3eec9
Mesut Tasci <slackermesut@gmail.com>
2016-10-02 02:24:56 +0300

    Initialize snipper if config file not found

--- snipper/snipper.py
+++ snipper/snipper.py
@@ -1,4 +1,5 @@
 import os
+from os import path
 import json
 import getpass
 import logging
@@ -8,7 +9,9 @@ import click
 
 from api import Snippet
 
-DEFAULT_SNIPPET_HOME = '~/.snippets'
+DEFAULT_SNIPPER_HOME = path.expanduser('~/.snippets')
+DEFAULT_SNIPPER_CONFIG = path.join(DEFAULT_SNIPPER_HOME, 'config.json')
+
 
 logger = logging.getLogger('snipper')
 logger.setLevel(logging.DEBUG) #TODO: set with --verbose param
@@ -19,9 +22,11 @@ logger.addHandler(ch)
 class SnipperConfig(object):
 
     def __init__(self, file):
-        self.file = os.path.expanduser(file)
+        self.file = file
         self.config = {}
-        conf_content = json.loads(open(self.file, 'r').read())
+
+        with open(self.file, 'r') as f:
+            conf_content = json.loads(f.read())
 
         for key, value in conf_content.items():
             self.config[key] = value
@@ -33,29 +38,29 @@ class SnipperConfig(object):
         self.config[key] = value
 
     def save_to_file(self):
-
         with open(self.file, 'w') as f:
             f.write(json.dumps(self.config))
 
         logger.info('Config file updated: {}'.format(self.file))
 
+    def file_exists(self):
+        return path.exists(self.file)
+
 pass_config = click.make_pass_decorator(SnipperConfig)
 
 
 @click.group()
-@click.option('--home', default=DEFAULT_SNIPPET_HOME, type=click.Path())
-@click.option('--config-file', default='{}/config.json'.format(DEFAULT_SNIPPET_HOME), type=click.Path())
+@click.option('--home', default=DEFAULT_SNIPPER_HOME, type=click.Path())
+@click.option('--config-file', default=DEFAULT_SNIPPER_CONFIG, type=click.Path())
 @click.pass_context
-def cli(ctx, home, config_file):
-
-    if  not os.path.exists(config_file):
-        click.echo('You are using snipper first time?')
-        click.echo('Call `snipper init` command')
-        ctx.exit()
+def cli(ctx, home, config_file, **kwargs):
 
     # Create a SnippetConfig object and remember it as as the context object.  From
     # this point onwards other commands can refer to it by using the
     # @pass_config decorator.
+    if not path.exists(config_file):
+        init_snipper(home=home)
+
     config = SnipperConfig(config_file)
     config.set('snippet_home', home)
 
@@ -65,24 +70,43 @@ def cli(ctx, home, config_file):
     api.set_config(config)
 
 
-@cli.command(name='init')
-@pass_config
-def init_snipper(config):
+def init_snipper(home):
+    config_file = path.join(home, 'config.json')
 
-    home = click.prompt('Where to keep snippets on local', default=DEFAULT_SNIPPET_HOME)
+    home = click.prompt('Where to keep snippets on local', default=home)
     username = click.prompt('Bitbucket username')
     click.echo('Password using for authenticating to Bitbucket API. You can create an app password for on Bitbucket account settings page.')
     password = getpass.getpass('Bitbucket Password:')
 
+    # Create snippet home dir
+    if not path.exists(home):
+        os.makedirs(home)
+
+    # Create config file
+
+    if not path.exists(config_file):
+        with open(config_file, 'w+') as f:
+            f.write('{}')
+
+
+    config = SnipperConfig(config_file)
     config.set('snippet_home', home)
     config.set('username', username)
     config.set('password', password)
     config.save_to_file()
 
+@cli.command(name='init')
+@click.option('--home', default=DEFAULT_SNIPPER_HOME, type=click.Path())
+def init(home, **kwargs):
+    init_snipper(home)
+
+
 
 @cli.command(name='ls')
 @pass_config
-def list_snippets(config,  **kwargs):
+@click.pass_context
+def list_snippets(context, config,  **kwargs):
+
     click.echo('List snippets')
 
 
