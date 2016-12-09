4a5d633764523ce2f8ffb19f32cb6508c831449e
Mesut Tasci <slackermesut@gmail.com>
2016-12-10 00:19:51 +0200

    Api method for creating snippet added

--- snipper/api.py
+++ snipper/api.py
@@ -29,3 +29,30 @@ class SnippetApi(BitbucketApi):
     def get_all(self):
         res = self.get(self.config.get('username'))
         return res.json()
+
+    def make_payload(self, is_private, title, scm, **kwargs):
+        payload = {}
+
+        if is_private is not None:
+            payload.update({'is_private': is_private})
+        if title is not None:
+            payload.update({'title': title})
+        if scm is not None:
+            payload.update({'scm': scm})
+
+        return payload
+
+    def create_snippet(self, files, is_private, title, scm):
+
+        username = self.config.get('username')
+        password = self.config.get('password')
+
+        payload = self.make_payload(is_private, title, scm)
+        response = requests.post(
+            self.base_url,
+            data=payload,
+            files=files,
+            auth=(username, password)
+        )
+
+        return response
