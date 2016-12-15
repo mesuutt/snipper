ed6126511db04d9818739ef24e03aabdcaa5e365
Mesut Tasci <slackermesut@gmail.com>
2016-12-15 21:56:15 +0300

    Move py files to snipper/ dir

--- snipper/snipper/completer.py
+++ snipper/snipper/completer.py
@@ -6,28 +6,20 @@ from prompt_toolkit.completion import Completer, Completion
 
 from snippet import Snippet
 
+
+__all__ = (
+    'SnippetFilesCompleter',
+)
+
+
 class PathCompleter(Completer):
-    paths = []
-
-    def __init__(self, config):
-        super(PathCompleter, self).__init__()
-
-        with open(os.path.join(config.config.get('metadata_file')), 'r') as file:
-            data = json.loads(file.read())
-            for item in data['values']:
-                snippet_id = item['id']
-                snippet = Snippet(config, item['owner']['username'], snippet_id)
-                file_dir = os.path.split(snippet.repo_path)[1]
-
-                for file_name in snippet.get_files():
-                    file_path_relative = os.path.join(item['owner']['username'], file_dir, file_name)
-                    self.paths.append(file_path_relative)
+    collection = []
 
     def get_completions(self, document, complete_event):
         # https://github.com/dbcli/pgcli/blob/master/pgcli/pgcompleter.py#L336
         word_before_cursor = document.get_word_before_cursor(WORD=True)
         text_len = len(word_before_cursor)
-        collection = self.fuzzyfinder(word_before_cursor, self.paths)
+        collection = self.fuzzyfinder(word_before_cursor, self.collection)
         matches = []
 
         for item in collection:
