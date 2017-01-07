3ada4cdeb3b85b1e9633a46c653e54c6431b3603
Mesut Tasci <slackermesut@gmail.com>
2017-01-07 23:59:06 +0300

    Tests for api.py added

---
+++ snipper/tests/test_api.py
@@ -0,0 +1,67 @@
+import os
+import json
+import unittest
+import configparser
+
+import httpretty
+
+from snipper.api import SnippetApi
+# from snipper.tests import config, TEST_DIR
+
+TEST_DIR = os.path.dirname(__file__)
+
+config = configparser.ConfigParser()
+config.read(os.path.join(TEST_DIR, 'snipperrc'))
+
+
+class BitbucketApiTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.api = SnippetApi(config)
+
+    def test_build_endpoint(self):
+        url = self.api.build_endpoint('mesuutt')
+        return self.assertEqual(url, os.path.join(SnippetApi.base_url, 'mesuutt'))
+
+    @httpretty.activate
+    def test_get_snippet_metadata(self):
+
+        url = self.api.build_endpoint('mesuutt')
+
+        with open(os.path.join(TEST_DIR, 'example_metadata.json'), 'r') as file:
+            example_metadata_content = file.read()
+
+        httpretty.register_uri(
+            httpretty.GET,
+            url,
+            body=example_metadata_content,
+            status=200,
+            content_type='application/json',
+        )
+
+        metadata = self.api.get_all()
+
+        self.assertEqual(metadata, json.loads(example_metadata_content))
+
+    def test_make_payload(self):
+        payload = self.api.make_payload(True, 'mytitle', 'git')
+
+        self.assertEqual(payload, {'is_private': True, 'scm': 'git', 'title': 'mytitle'})
+
+    @httpretty.activate
+    def test_create_snippet(self):
+        file_list = [('file', ('file.txt', 'content',),)]
+        payload = {'title': 'title', 'scm': 'git', 'is_private': True}
+        print(self.api.base_url)
+
+        httpretty.register_uri(
+            httpretty.POST,
+            self.api.base_url,
+            data=payload,
+            files=file_list,
+            status=201
+        )
+
+        response = self.api.create_snippet(file_list, True, 'mytitle', 'git')
+
+        self.assertEqual(response.status_code, 201)
